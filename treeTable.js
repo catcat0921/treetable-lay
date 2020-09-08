31107d28fac31b32ec5cc2dd6ee8e4222378643e
郭朝靖 <1141640389@qq.com>
2020-09-08 11:14:32 +0800

    update treeTable.js. 修改显示父级下拉箭头判断 使children标识的字段名返回值为"false"时不显示下拉箭头

--- treeTable.js
+++ treeTable.js
@@ -909,7 +909,7 @@ layui.define(['laytpl', 'form', 'util'], function (exports) {
             // 加箭头
             var haveChild = d[options.tree.haveChildName];
             if (haveChild === undefined) haveChild = d[options.tree.childName] && d[options.tree.childName].length > 0;
-            icon += ('<i class="ew-tree-table-arrow layui-icon' + (haveChild ? '' : ' ew-tree-table-arrow-hide'));
+            icon += ('<i class="ew-tree-table-arrow layui-icon' + (haveChild === true || haveChild === 'true' ? '' : ' ew-tree-table-arrow-hide'));
             icon += (' ' + (options.tree.arrowType || '') + '"></i>');
             // 加图标
             icon += options.tree.getIcon(d);
